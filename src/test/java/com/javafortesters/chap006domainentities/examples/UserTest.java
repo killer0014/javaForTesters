874b8c52e8f4c773579e9dd244e0d32f3627bb67
Lars Schraven <lars.schraven@duo.nl>
2017-04-05 17:13:40 +0200

    nieuwe oefening

--- src/test/java/com/javafortesters/chap006domainentities/examples/UserTest.java
+++ src/test/java/com/javafortesters/chap006domainentities/examples/UserTest.java
@@ -1,5 +1,15 @@
 package com.javafortesters.chap006domainentities.examples;
 
+import org.junit.Test;
+
+import com.javafortesters.domainentities.User;
+
 public class UserTest {
+
+    @Test
+        public void canConstructANewUser(){
+    	User user = new User();
+
+    }
 
 }
