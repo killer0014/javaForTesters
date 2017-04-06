533357142599c38517ce5621d7b6f96f56076a7b
Lars Schraven <lars.schraven@duo.nl>
2017-04-06 11:49:03 +0200

    new @test

--- src/test/java/com/javafortesters/chap006domainentities/examples/UserTest.java
+++ src/test/java/com/javafortesters/chap006domainentities/examples/UserTest.java
@@ -1,5 +1,7 @@
 package com.javafortesters.chap006domainentities.examples;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 import com.javafortesters.domainentities.User;
@@ -7,9 +9,16 @@ import com.javafortesters.domainentities.User;
 public class UserTest {
 
     @Test
-        public void canConstructANewUser(){
-    	User user = new User();
+    public void canConstructANewUser() {
+	User user = new User();
 
+    }
+
+    @Test
+    public void userHasDefaultUsernameAndPassword() {
+	User user = new User();
+	assertEquals("default username expected", "username", user.getUsername());
+	assertEquals("default password expected", "password", user.getPassword());
     }
 
 }
